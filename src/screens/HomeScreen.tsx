1678e6a168893c3ee5a6aa82be591c66f9674230
NguyenTamHy <nguyenhy1308@gmail.com>
2025-06-19 12:35:08 +0700

    Update HomeScreen and add logoDraw.png

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,73 @@
-import { View, Text, Button } from 'react-native';
-
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 export default function HomeScreen({ navigation }: any) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Draw" onPress={() => navigation.navigate('Draw')} />
-      <Button title="Go to LoginScreen" onPress={() => navigation.navigate('LoginScreen')} />
-      <Button title="Go to Register" onPress={() => navigation.navigate('Register')} />
-      <Button title="Go to Gallery" onPress={() => navigation.navigate('Gallery')} />
+    <View style={styles.container}>
+      {/* Logo */}
+      <Image
+        source={require('../../assets/logoDraw.png')} // đặt đúng đường dẫn
+        style={styles.logo}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.title}>🎨 Drawly App</Text>
+      <Text style={styles.subtitle}>Chào mừng bạn đến với không gian sáng tạo!</Text>
+
+      <TouchableOpacity
+        style={[styles.button, styles.loginButton]}
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        <Text style={styles.buttonText}>Đăng nhập</Text>
+      </TouchableOpacity>
+
+      
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFBE6',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#FFB300',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#444',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  button: {
+    width: '80%',
+    paddingVertical: 12,
+    borderRadius: 10,
+    marginVertical: 10,
+    alignItems: 'center',
+    elevation: 2,
+  },
+  loginButton: {
+    backgroundColor: '#FFD54F',
+  },
+  registerButton: {
+    backgroundColor: '#F9A825',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
